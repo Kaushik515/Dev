f38419034e09ea9da4268ad745afd543db208c7b
Kotha Kaushik <saikaushik515@gmail.com>
2026-02-19 19:07:49 +0530

    Latest Updated Day 3

---
+++ Sheets/Constructors.cpp
@@ -0,0 +1,23 @@
+#include "bits/stdc++.h"
+using namespace std;
+class Car{
+    public:
+    string brand,model;
+    int mileage=0;
+    void drive(int distance){
+        mileage += distance;
+    }
+    void show(){
+        cout<<"Brand: "<<brand<<'\n';
+        cout<<"Model: "<<model<<'\n';
+        cout<<"Distance driven: "<<mileage<<'\n';
+    }
+};
+int main(){
+    Car c1;
+    c1.brand = "Suzuki";
+    c1.model = "Breeza";
+    c1.drive(100);
+    c1.show();
+    return 0;
+}
