87c0bc88f665958d992ab1cfb490a936fb511806
Kaushik515 <saikaushik39@gmail.com>
2026-02-22 23:46:49 +0530

    Day 5 & 6 of Restart

---
+++ Sheets/Clone-graph.cpp
@@ -0,0 +1,64 @@
+#include "bits/stdc++.h"
+using namespace std;
+class Node {
+public:
+  int val;
+  vector<Node *> neighbors;
+  Node() {
+    val = 0;
+    neighbors = vector<Node *>();
+  }
+  Node(int _val) {
+    val = _val;
+    neighbors = vector<Node *>();
+  }
+  Node(int _val, vector<Node *> _neighbors) {
+    val = _val;
+    neighbors = _neighbors;
+  }
+};
+class Solution {
+public:
+  Node *cloneGraph(Node *node) {
+    if (node == NULL)
+      return NULL;
+    queue<Node *> q;
+    q.push(node);
+    Node *clone = new Node(node->val);
+    map<Node *, Node *> mp;
+    mp[node] = clone;
+    while (!q.empty()) {
+      auto it = q.front();
+      q.pop();
+      for (auto i : it->neighbors) {
+        if (mp.find(i) == mp.end()) {
+          Node *cloneNode = new Node(i->val);
+          mp[i] = cloneNode;
+          q.push(i);
+        }
+        mp[it]->neighbors.push_back(mp[i]);
+      }
+    }
+    return clone;
+  }
+};
+int main() {
+  Solution sol;
+  int n;
+  cin >> n;
+  vector<Node *> nodes(n);
+  for (int i = 0; i < n; i++) {
+    nodes[i] = new Node(i + 1);
+  }
+  for (int i = 0; i < n; i++) {
+    int k;
+    cin >> k;
+    for (int j = 0; j < k; j++) {
+      int neighbor;
+      cin >> neighbor;
+      nodes[i]->neighbors.push_back(nodes[neighbor - 1]);
+    }
+  }
+  Node *clone = sol.cloneGraph(nodes[0]);
+  return 0;
+}
---
+++ Sheets/WordLadder.cpp
@@ -0,0 +1,49 @@
+#include "bits/stdc++.h"
+using namespace std;
+class Solution {
+public:
+  int ladderLength(string beginWord, string endWord, vector<string> &wordList) {
+    queue<pair<string, int>> q;
+    q.push({beginWord, 1});
+    set<string> st;
+    for (string s : wordList)
+      st.insert(s);
+    if (st.find(endWord) == st.end())
+      return 0;
+    while (!q.empty()) {
+      auto it = q.front();
+      q.pop();
+      string s = it.first;
+      int seq = it.second;
+      if (s == endWord)
+        return seq;
+      for (int i = 0; i < s.size(); i++) {
+        string str = s;
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+          str[i] = ch;
+          if (st.find(str) != st.end()) {
+            st.erase(str);
+            q.push({str, seq + 1});
+          }
+        }
+      }
+    }
+    return 0;
+  }
+};
+int main() {
+  Solution sol;
+  int t;
+  cin >> t;
+  while (t--) {
+    string beginWord, endWord;
+    cin >> beginWord >> endWord;
+    int n;
+    cin >> n;
+    vector<string> wordList(n);
+    for (int i = 0; i < n; i++)
+      cin >> wordList[i];
+    cout << sol.ladderLength(beginWord, endWord, wordList) << endl;
+  }
+  return 0;
+}
